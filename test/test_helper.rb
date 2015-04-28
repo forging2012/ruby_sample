89cf56688a5bee516663311689584604b135aff7
fugeng1994 <223328084@qq.com>
2015-04-28 11:27:17 +0800

    简化首页布局

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
+# 使用单元测试时不同结果 返回不同颜色
+require "minitest/reporters"
+Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
